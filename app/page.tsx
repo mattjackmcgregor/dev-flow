f39d5941f8d44ebae7c724ecfef9c1b94cb4c3b4
matt mcgregor <mattjackmcgregor@gmail.com>
2023-09-28 17:11:23 +1300

    implement auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">nextjs we&apos;re coming!</h1>
-      <h1 className="h2-bold">nextjs we&apos;re coming!</h1>
-      <h1 className="h3-bold">nextjs we&apos;re coming!</h1>
-    </div>
-  );
-}
