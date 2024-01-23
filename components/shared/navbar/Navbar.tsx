d3fc54ff0888463854c7f0dd17cd5cd4dacc9c0b
matt mcgregor <mattjackmcgregor@gmail.com>
2024-01-23 18:05:00 +1300

    implement theme switcher, and left sidebar, responsiveness for desktop, tablet and mobile

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,10 +2,13 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -17,12 +20,15 @@ const Navbar = () => {
           Dev<span className="text-primary-500">Overflow</span>
         </p>
       </Link>
-      GlobalSearch
+
+      <GlobalSearch />
+
       <div className="flex-between gap-5">
-        themeLogo
+        <Theme />
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
         </SignedIn>
+        <MobileNav />
       </div>
     </nav>
   );
