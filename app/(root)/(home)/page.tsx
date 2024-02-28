1f0440c58d7154e7031a8f3abe747963e8768799
matt mcgregor <mattjackmcgregor@gmail.com>
2024-02-28 14:38:38 +1300

    created localSearchBar component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,24 @@
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link
+          className="flex justify-end max-sm:w-full"
+          href={"/all-questions"}
+        >
+          <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch />
+      </div>
+    </>
+  );
 }
